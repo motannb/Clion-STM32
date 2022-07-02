b206c806dbe54a097970f8ae80aa632fc8d26cde
motannb <2286737370@qq.com>
2022-07-02 11:05:58 +0800

    add main.c/main.h

---
+++ main.c
@@ -0,0 +1,11 @@
+//
+// Created by 22867 on 2022/7/2.
+//
+
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello World!");
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by 22867 on 2022/7/2.
+//
+
+#ifndef GITDEMO_MAIN_H
+#define GITDEMO_MAIN_H
+
+#endif //GITDEMO_MAIN_H
